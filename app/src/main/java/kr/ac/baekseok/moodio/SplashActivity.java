45e6d4ea304f140118d4c66f9ccb267f69daeb2f
bmbx08 <bmbx08@bu.ac.kr>
2025-06-07 00:58:17 +0900

    First commit

---
+++ app/src/main/java/kr/ac/baekseok/moodio/SplashActivity.java
@@ -0,0 +1,29 @@
+package kr.ac.baekseok.moodio;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 5000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // 5초 후 로그인 화면으로 이동
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); // 스플래시 액티비티 종료
+            }
+        }, SPLASH_DELAY);
+    }
+}
