95d2575396925f24dbc8f3ec77f552c949e065c3
bmbx08 <bmbx08@bu.ac.kr>
2025-06-07 01:33:58 +0900

    added splash, login, register xml

--- app/src/main/java/kr/ac/baekseok/moodio/SplashActivity.java
+++ app/src/main/java/kr/ac/baekseok/moodio/SplashActivity.java
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static final int SPLASH_DELAY = 5000;
+    private static final int SPLASH_DELAY = 3000;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -17,13 +17,13 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         // 5초 후 로그인 화면으로 이동
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-//                startActivity(intent);
-//                finish(); // 스플래시 액티비티 종료
-//            }
-//        }, SPLASH_DELAY);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); // 스플래시 액티비티 종료
+            }
+        }, SPLASH_DELAY);
     }
 }
